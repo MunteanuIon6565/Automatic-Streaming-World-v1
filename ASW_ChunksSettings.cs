fdb872c9821f32d059885bd98b994d6d16e9faf0
MunteanuIon6565 <ion.munteanu1@midnight.works>
2025-01-16 11:39:28 +0200

    .

---
+++ ASW_ChunksSettings.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace AUTOMATIC_STREAMING_WORLD
+{
+    [CreateAssetMenu(fileName = "Chunks Sorter Settings", menuName = "STREAMING WORLD SYSTEM/Chunk Sorter Settings", order = 100)]
+    public class ASW_ChunksSettings : ScriptableObject
+    {
+        [Header("Chunk Settings")] 
+        public Vector3 ChunkSize { get; private set; } = new Vector3(100, 100, 100);
+
+        [Header("Runtime Settings")] 
+        public float LoopTimeCheckDistance = 5f;
+        
+        [Header("Editor Settings")] 
+        public float LoopTimeAutomaticSortEditor = 5f;
+    }
+}
