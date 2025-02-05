826112fa3e0201e03edccd0a9a1250ac9c19873c
MunteanuIon6565 <ion.munteanu1@midnight.works>
2025-02-05 15:00:54 +0200

    Update AWS_FloatingOriginRigidbody.cs

--- Runtime/Scripts/FloatingOriginSystem/FloatingOriginObjectsType/AWS_FloatingOriginRigidbody.cs
+++ Runtime/Scripts/FloatingOriginSystem/FloatingOriginObjectsType/AWS_FloatingOriginRigidbody.cs
@@ -12,7 +12,14 @@ namespace AUTOMATIC_WORLD_STREAMING.FloatingOriginObjectsType
         {
             base.ShiftPosition(positionToShift);
             
+            Vector3 velocity = m_rigidbody.velocity;
+            bool isKinematic = m_rigidbody.isKinematic;
+            
+            m_rigidbody.isKinematic = true;
             m_rigidbody.position += positionToShift;
+            m_rigidbody.isKinematic = isKinematic;
+            
+            m_rigidbody.velocity = velocity;
         }
 
         protected override void Start()
